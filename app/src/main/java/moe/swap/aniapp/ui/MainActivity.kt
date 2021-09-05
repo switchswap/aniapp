3a9214e927fe5631460323cc71a7e6e9699e4cea
Swap <sp70215@gmail.com>
2021-09-05 16:32:33 -0700

    Move `MainActivity` into `ui` package

--- app/src/main/java/moe/swap/aniapp/ui/MainActivity.kt
+++ app/src/main/java/moe/swap/aniapp/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package moe.swap.aniapp
+package moe.swap.aniapp.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import moe.swap.aniapp.R
 import moe.swap.aniapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -25,8 +26,9 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_library,
-            R.id.navigation_downloads, R.id.navigation_settings))
+            R.id.navigation_home, R.id.navigation_search, R.id.navigation_library,
+            R.id.navigation_downloads, R.id.navigation_settings
+        ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
