a5fc8f18cefe00c5b0136b7a16311065d13a29f2
Swap <sp70215@gmail.com>
2021-12-20 22:53:01 -0800

    Remove GraphQL test
    I broke this so I'm removing it for now

--- app/src/androidTest/java/moe/swap/aniapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/moe/swap/aniapp/ExampleInstrumentedTest.kt
@@ -2,10 +2,6 @@ package moe.swap.aniapp
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import moe.swap.aniapp.network.anilist.AnilistRepository
-import moe.swap.aniapp.network.util.getError
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,15 +20,5 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("moe.swap.aniapp", appContext.packageName)
-    }
-
-    @Test
-    fun testGetTrendingAnime() {
-        val anilistRepository = AnilistRepository()
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        runBlocking {
-            val request = anilistRepository.getTrendingAnime(appContext, "FALL", 2021)
-            assertEquals(200, request.code())
-        }
     }
 }
