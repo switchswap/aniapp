0dcc77740823300003a59c11b3b5d8b17d427a8e
Swap <sp70215@gmail.com>
2022-01-05 05:25:21 -0800

    Update variable name to match previous rename

--- app/src/main/java/moe/swap/aniapp/network/anilist/AnilistService.kt
+++ app/src/main/java/moe/swap/aniapp/network/anilist/AnilistService.kt
@@ -2,7 +2,7 @@ package moe.swap.aniapp.network.anilist
 
 import io.github.wax911.library.annotation.GraphQuery
 import io.github.wax911.library.model.request.QueryContainerBuilder
-import moe.swap.aniapp.network.anilist.models.AnimeQueryResult
+import moe.swap.aniapp.network.anilist.models.AnilistSearchResponse
 import moe.swap.aniapp.network.util.GraphContainer
 import retrofit2.Response
 import retrofit2.http.Body
@@ -13,7 +13,7 @@ interface AnilistService {
     @GraphQuery("AnimeSearch")
     suspend fun searchAnime(
         @Body builder: QueryContainerBuilder
-    ): Response<GraphContainer<AnimeQueryResult>>
+    ): Response<GraphContainer<AnilistSearchResponse>>
 
     companion object {
         const val API_URL = "https://graphql.anilist.co/"
