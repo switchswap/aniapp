2f7865d9b53946265af95683ade06880cbd5952c
Swap <sp70215@gmail.com>
2022-01-05 11:06:28 -0800

    Restructure class to match incoming JSON
    The `data` variable is not needed since the top level item will be `GraphContainer` which already has `data`.

--- app/src/main/java/moe/swap/aniapp/network/anilist/models/AnilistSearchResponse.kt
+++ app/src/main/java/moe/swap/aniapp/network/anilist/models/AnilistSearchResponse.kt
@@ -3,24 +3,19 @@ package moe.swap.aniapp.network.anilist.models
 import com.google.gson.annotations.SerializedName
 
 data class AnilistSearchResponse(
-    val `data`: Data
+    @SerializedName("Page")
+    val page: Page
 ) {
-    data class Data(
-        // TODO: serialized name Page
-        @SerializedName("Page")
-        val page: Page
+    data class Page(
+        val media: List<AnimeAnilistEntity>,
+        val pageInfo: PageInfo
     ) {
-        data class Page(
-            val media: List<AnimeAnilistEntity>,
-            val pageInfo: PageInfo
-        ) {
-            data class PageInfo(
-                val currentPage: Int,
-                val hasNextPage: Boolean,
-                val lastPage: Int,
-                val perPage: Int,
-                val total: Int
-            )
-        }
+        data class PageInfo(
+            val currentPage: Int,
+            val hasNextPage: Boolean,
+            val lastPage: Int,
+            val perPage: Int,
+            val total: Int
+        )
     }
 }
